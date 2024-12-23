7ab8d57f9a54d6a0ccd79539877f6827a2eb16e4
chris <krzysiek002@gmail.com>
2024-12-23 22:21:07 +0100

    refactoring

--- src/database.py
+++ src/database.py
@@ -14,3 +14,18 @@ def fetch_latest_url():
     url_tuple = cursor.fetchone()
     conn.close()
     return url_tuple[0]
+
+def fetch_all_urls():
+    """Fetch all URLs from the database."""
+    conn = psycopg2.connect(
+        dbname='postgres',
+        user='postgres',
+        password='mysecretpassword',
+        host='localhost',
+        port='5432'
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT long_url FROM url")
+    url_tuples = cursor.fetchall()
+    conn.close()
+    return [url_tuple[0] for url_tuple in url_tuples]
--- src/scraper.py
+++ src/scraper.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_website(url):
+def scrape_website(url,total_context=1000):
     """Scrape the website and return the text content."""
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     text_content = soup.get_text()
-    return text_content[:100]
+    return text_content[:total_context]
