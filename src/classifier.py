69e8939de3394c11c2259e9537593a4485d15245
chris <krzysiek002@gmail.com>
2024-12-23 22:21:07 +0100

    refactoring

--- src/classifier.py
+++ src/classifier.py
@@ -1,19 +1,30 @@
 import requests
 
-def classify_content(text_content):
+def classify_content(text_content, model="llama3.2:3b"):
     """Classify the content using the local Ollama instance."""
     url = 'http://localhost:11434/v1/completions'
     headers = {
         'Content-Type': 'application/json',
     }
-    prompt = text_content + "classify in only one word what type of content is on this website, return only one word"
+    prompt = text_content + "classify what category of website is it, return category in 1 word only"
     payload = {
         "prompt": prompt,
-        "model": "llama3.1:latest"
+        "model": model
     }
+    # response = requests.post(url, headers=headers, json=payload)
+    # if response.status_code == 200:
+    #     data = response.json()
+    #     return data['choices'][0]['text']
+    # else:
+    #     raise Exception(f"Request failed with status code {response.status_code}")
+    i=0
     response = requests.post(url, headers=headers, json=payload)
-    if response.status_code == 200:
+    while response.status_code == 200 and i<10:
+        i+=1
+        print(i)
         data = response.json()
-        return data['choices'][0]['text']
-    else:
-        raise Exception(f"Request failed with status code {response.status_code}")
+        print(data['choices'][0]['text'])
+        if len(data['choices'][0]['text'].split()) == 1 :
+
+            return data['choices'][0]['text']
+        response = requests.post(url, headers=headers, json=payload)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
-from database import fetch_latest_url
+from database import fetch_latest_url, fetch_all_urls
 from scraper import scrape_website
 from classifier import classify_content
 
 if __name__ == "__main__":
-    url = fetch_latest_url()
-    limited_text_content = scrape_website(url)
-    print(limited_text_content)
-    classification = classify_content(limited_text_content)
-    print(classification)
+    #url = fetch_latest_url()
+    urls = fetch_all_urls()
+    for url in urls:
+     limited_text_content = scrape_website(url)
+     classification = classify_content(limited_text_content)
+     print(classification + " - " + url)
+    # url ="https://www.google.com"
+    # limited_text_content = scrape_website(url)
+    # classification = classify_content(limited_text_content)
+    # print(classification + " - " + url)
